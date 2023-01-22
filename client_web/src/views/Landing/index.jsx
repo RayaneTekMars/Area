e0eadb02dd73895682c08f497007ec3c317a9275
Tony Bano <tony.bano@epitech.eu>
2023-01-22 11:45:15 +0100

    [Add] : React router + landing page

---
+++ client_web/src/views/Landing/index.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Landing from './landing.jsx';
+
+function LandingPage() {
+  return (
+    <React.Fragment>
+        <Landing />
+    </React.Fragment>
+  );
+}
+
+export default LandingPage;
