e96b5f56c2296a9dc3ba46671c1a03bea14ab495
Prime <theo.tarallo@epitech.eu>
2023-01-23 12:12:55 +0100

    [Add] - Custom fonts.

--- client_mobile/App.js
+++ client_mobile/App.js
@@ -1,7 +1,19 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Inter-Black': require('./assets/fonts/Inter-Black.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'Inter-ExtraBold': require('./assets/fonts/Inter-ExtraBold.ttf')
+  });
+
+  if (!fontsLoaded) {
+    return (null);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Votre nouveau compagnon au quotidien</Text>
@@ -19,5 +31,6 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'white',
+    fontFamily: 'Inter-Black',
   }
 });
