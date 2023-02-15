979763cfa2f1d42582d0baaa28e002b87b96dc4a
Rayane ELOUDJEDI <rayane.eloudjedi@epitech.eu>
2023-02-15 11:38:42 +0100

    Update subscriptions controller to add Github support

--- server/src/subscriptions/subscriptions.controller.ts
+++ server/src/subscriptions/subscriptions.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards } from '@nestjs/common'
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ApiBadRequestResponse, ApiOkResponse } from '@nestjs/swagger';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { CurrentUser } from '../common/decorators/current-user.decorator';
-import { Account } from '../accounts/entities/account.entity';
-import { GetSubscriptionsResDto, SubscriptionResDto, SubscriptionUrlResDto } from './dto/subscriptions.res.dto';
-import { SubscriptionReqDto } from './dto/subscriptions.req.dto';
-import { SubscriptionsService } from './subscriptions.service';
-import { TwitterSubscribeService } from './services/twitter.sub.service';
+import { Body, Controller, Delete, Get, HttpCode, Post, Put, UseGuards } from '@nestjs/common'
+import { ApiBearerAuth, ApiOperation, ApiTags, ApiBadRequestResponse, ApiOkResponse } from '@nestjs/swagger'
+import { GetSubscriptionsResDto, SubscriptionResDto, SubscriptionUrlResDto } from './dto/subscriptions.res.dto'
+import { SubscriptionReqDto } from './dto/subscriptions.req.dto'
+import { SubscriptionsService } from './subscriptions.service'
+import { TwitterSubscribeService } from './services/twitter.sub.service'
+import { GithubSubscribeService } from './services/github.sub.service'
+import { Account } from '../accounts/entities/account.entity'
+import { CurrentUser } from '../common/decorators/current-user.decorator'
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'
 
 @Controller('subscriptions')
 @ApiTags('Subscriptions')
@@ -17,7 +17,8 @@ export class SubscriptionsController {
     constructor(
         public readonly subscriptionsService: SubscriptionsService,
         public readonly twitterSubscribeService: TwitterSubscribeService,
-    ) { }
+        public readonly githubSubscribeService: GithubSubscribeService
+    ) {}
 
     @Get('')
     @UseGuards(JwtAuthGuard)
@@ -26,11 +27,11 @@ export class SubscriptionsController {
     @ApiOkResponse({ type: GetSubscriptionsResDto })
     @ApiBadRequestResponse()
     async getSubscriptions(@CurrentUser() user: Account): Promise<GetSubscriptionsResDto> {
-        const subscriptions = await this.subscriptionsService.getSubscriptions(user.id);
+        const subscriptions = await this.subscriptionsService.getSubscriptions(user.id)
         return {
             status: 'success',
             data: subscriptions
-        };
+        }
     }
 
     @Get('twitter')
@@ -40,14 +41,14 @@ export class SubscriptionsController {
     @ApiOkResponse({ type: SubscriptionUrlResDto })
     @ApiBadRequestResponse()
     async getTwitterSubscription(@CurrentUser() user: Account): Promise<SubscriptionUrlResDto> {
-        const url = await this.twitterSubscribeService.getAuthorizeUrl(user.id);
+        const url = await this.twitterSubscribeService.getAuthorizeUrl(user.id)
 
         return {
             status: 'success',
             data: {
-                url,
+                url
             }
-        };
+        }
     }
 
     @Post('twitter')
@@ -57,15 +58,15 @@ export class SubscriptionsController {
     @ApiOkResponse({ type: SubscriptionResDto })
     @ApiBadRequestResponse()
     async createTwitterSubscription(@CurrentUser() user: Account, @Body() body: SubscriptionReqDto): Promise<SubscriptionResDto> {
-        const { accessToken, refreshToken, expiresIn } = await this.twitterSubscribeService.authorize(user.id, body.code);
-        const subscription = await this.subscriptionsService.createSubscription('Twitter', user, accessToken, refreshToken!, expiresIn);
+        const { accessToken, refreshToken, expiresIn } = await this.twitterSubscribeService.authorize(user.id, body.code)
+        const subscription = await this.subscriptionsService.createSubscription('Twitter', user, accessToken, refreshToken!, expiresIn)
 
         return {
             status: 'success',
             data: {
-                id: subscription.id,
+                id: subscription.id
             }
-        };
+        }
     }
 
     @Put('twitter')
@@ -75,21 +76,21 @@ export class SubscriptionsController {
     @ApiOkResponse({ type: SubscriptionResDto })
     @ApiBadRequestResponse()
     async updateTwitterSubscription(@CurrentUser() user: Account): Promise<SubscriptionResDto> {
-        const subscription = await this.subscriptionsService.getSubscription('Twitter', user.id);
+        const subscription = await this.subscriptionsService.getSubscription('Twitter', user.id)
 
-        if (!subscription) {
-            throw new Error('Subscription not found');
-        }
+        if (!subscription)
+            throw new Error('Subscription not found')
 
-        const { accessToken, refreshToken, expiresIn } = await this.twitterSubscribeService.refreshAccessToken(subscription.refreshToken!);
-        await this.subscriptionsService.updateSubscription('Twitter', user.id, accessToken, refreshToken!, expiresIn);
+
+        const { accessToken, refreshToken, expiresIn } = await this.twitterSubscribeService.refreshAccessToken(subscription.refreshToken)
+        await this.subscriptionsService.updateSubscription('Twitter', user.id, accessToken, refreshToken!, expiresIn)
 
         return {
             status: 'success',
             data: {
-                id: subscription.id,
+                id: subscription.id
             }
-        };
+        }
     }
 
     @Delete('twitter')
@@ -99,20 +100,54 @@ export class SubscriptionsController {
     @ApiOkResponse({ type: SubscriptionResDto })
     @ApiBadRequestResponse()
     async deleteTwitterSubscription(@CurrentUser() user: Account): Promise<SubscriptionResDto> {
-        const subscription = await this.subscriptionsService.deleteSubscription('Twitter', user.id);
+        const subscription = await this.subscriptionsService.deleteSubscription('Twitter', user.id)
 
-        if (!subscription) {
-            throw new Error('Subscription not found');
-        }
+        if (!subscription)
+            throw new Error('Subscription not found')
 
-        this.twitterSubscribeService.revokeAccessToken(subscription.refreshToken!);
+
+        this.twitterSubscribeService.revokeAccessToken(subscription.refreshToken)
 
         return {
             status: 'success',
             data: {
-                id: subscription.id,
+                id: subscription.id
             }
-        };
+        }
     }
 
+    @Get('github')
+    @UseGuards(JwtAuthGuard)
+    @HttpCode(200)
+    @ApiOperation({ summary: 'Get an AuthLink for Github' })
+    @ApiOkResponse({ type: SubscriptionUrlResDto })
+    @ApiBadRequestResponse()
+    async getGithubSubscription(): Promise<SubscriptionUrlResDto> {
+        const url = await this.githubSubscribeService.getAuthorizeUrl()
+
+        return {
+            status: 'success',
+            data: {
+                url
+            }
+        }
+    }
+
+    @Post('github')
+    @UseGuards(JwtAuthGuard)
+    @HttpCode(200)
+    @ApiOperation({ summary: 'Create a subscription to Github' })
+    @ApiOkResponse({ type: SubscriptionResDto })
+    @ApiBadRequestResponse()
+    async createGithubSubscription(@CurrentUser() user: Account, @Body() body: SubscriptionReqDto): Promise<SubscriptionResDto> {
+        const { accessToken, refreshToken, expiresIn } = await this.twitterSubscribeService.authorize(user.id, body.code)
+        const subscription = await this.subscriptionsService.createSubscription('Github', user, accessToken, refreshToken!, expiresIn)
+
+        return {
+            status: 'success',
+            data: {
+                id: subscription.id
+            }
+        }
+    }
 }
