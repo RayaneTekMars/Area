a44346c5225428b5e41aaafcb49afb6aa99db6d9
Rayane ELOUDJEDI <rayane.eloudjedi@epitech.eu>
2023-02-22 22:27:02 +0100

    Add Spotify and Twitch Oauth buttons and pages

---
+++ client_web/src/views/Oauth/spotify/index.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Spotify from './spotify.jsx';
+
+function SpotifyPage() {
+  return (
+    <div>
+        <Spotify />
+    </div>
+  );
+}
+
+export default SpotifyPage;
