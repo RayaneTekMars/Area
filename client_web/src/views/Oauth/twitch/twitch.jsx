1ee56d53aaaf2ae71406f1e53dce358155d5e9b4
Rayane ELOUDJEDI <rayane.eloudjedi@epitech.eu>
2023-02-22 22:27:15 +0100

    Add Twitch OAuth page and logic

---
+++ client_web/src/views/Oauth/twitch/twitch.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function TwitchPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('jwt');
+    if (!token)
+      throw new Error('Le token JWT est manquant');
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const code = urlSearchParams.get('code');
+    if (!code)
+      throw new Error('Le code est manquant dans l\'URL');
+
+    fetch('http://localhost:8080/subscriptions/twitch', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ code }),
+    })
+      .then(response => {
+        if (!response.ok)
+          throw new Error('La requête a échoué');
+      })
+      .then(data => {
+        console.log(data);
+        navigate('/home');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [navigate]);
+
+  return <div>En attente de la réponse...</div>;
+}
+
+export default TwitchPage;
