24ab825e677c938c1c99d7a0106df94b741ffbc0
Tony Bano <tony.bano@epitech.eu>
2023-03-03 11:43:50 +0100

    [Add] : Add Spotify

--- client_web/src/components/discordButton.jsx
+++ client_web/src/components/discordButton.jsx
@@ -17,7 +17,7 @@ const DiscordButton = () => {
       headers,
     };
 
-    fetch("https://api.automateme.fr/subscriptions/Discord", requestOptions)
+    fetch("http://localhost:8080/subscriptions/Discord", requestOptions)
       .then((res) => res.json())
       .then((res) => {
         setUrl(res.data.url);
